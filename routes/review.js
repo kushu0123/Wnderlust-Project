c991ae6a480b6f68df9e55e6bef84adb8345e6e0
Delta Demo <lata83509@gmail.com>
2026-08-06 02:01:47 -0700

    Fix listing model import path

--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
-const Listing = require("../models/Listing");
+const Listing = require("../models/listing");
 const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 const reviewController = require("../controllers/review.js");
 
